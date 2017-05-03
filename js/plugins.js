18733f35b6ae940ccef6f3cf14afb83f6ca5497c
AVorona <derian911@gmail.com>
2017-05-03 21:21:43 +0300

    first commit. Time: 3h45min

---
+++ js/plugins.js
@@ -0,0 +1,43 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+$(document).ready(function() {
+          $('.plus-toggle').click(function(){
+            //get collapse content selector
+            var collapse_content_selector = $(this).attr('href');
+
+            //make the collapse content to be shown or hide
+            var toggle_switch = $(this);
+            $(collapse_content_selector).toggle(function(){
+              if($(this).css('display')=='none'){
+                                //change the button label to be 'Show'
+                toggle_switch.html('+');
+              }else{
+                                //change the button label to be 'Hide'
+                toggle_switch.html('-');
+              }
+            });
+          });
+
+        });
