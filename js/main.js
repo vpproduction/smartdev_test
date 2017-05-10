a3006f33d564ef608acdf20e40bab075442fa3d8
AVorona <derian911@gmail.com>
2017-05-10 17:59:12 +0300

    version 1.0

--- js/main.js
+++ js/main.js
@@ -3,45 +3,42 @@
 var modal = document.getElementById('shopsModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("modalTrigger");
+var btn1 = document.getElementById("modalTrigger");
+//Extra button in the footer
+var btn2 =document.getElementById("modalTrigger2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+btn1.onclick = function() {
     modal.style.display = "block";
 }
-
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
-
-
 //bxslider library usage for responsive slider
 //bxSlider
-$('.bxslider').bxSlider({
-  buildPager: function(slideIndex){
-  	auto:true;
-    switch(slideIndex){
-      case 0:
-        return '<img src="img/content/thumbs/dress-1.jpg">';
-      case 1:
-        return '<img src="img/content/thumbs/dress-2.jpg">';
-      case 2:
-        return '<img src="img/content/thumbs/dress-3.jpg">';
-        case 3:
-        return '<img src="img/content/thumbs/dress-4.jpg">';
-    }
-  }
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    mode:'fade',
+    speed:400,
+    pagerCustom: '#bx-pager',
+    controls: true,
+    infiniteLoop: false,
+    hideControlOnEnd: true,
+  });
 
 });
+
+
